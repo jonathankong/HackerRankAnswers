a7f8c2fb4bcd5c894d53f6da7e8e4d2021dd3069
Jonathan Ong <jko665_000@msn.com>
2024-09-03 18:11:37 -0400

    Update FlippingMatrix.py
    Updated with documentation on how I figured it out

--- 1 Week Preparation Kit/Day 2/FlippingMatrix.py
+++ 1 Week Preparation Kit/Day 2/FlippingMatrix.py
@@ -3,6 +3,15 @@ def FlippingMatrix(arr):
     #left quadrant size
     maxInd = len(arr) - 1 
 
+
+    #only certain values can be moved into the top left quadrant of the square matrix with only flipping rows or columns.abs
+    #ie [1, 2, 2, 1]
+    #   [3, 4, 4, 3]
+    #   [3, 4, 4, 3]
+    #   [1, 2, 2, 1]
+    #in this matrix, the numbers above show the groups of numbers that must be compared to determine what will show up in the top left submatrix.
+    #ie. only the values in the "1" group ever show up in the top left corner of the submatrix.  This means that we just need to find the max value in 
+    #that group and add it to a running sum
     for i in range(int(len(arr)/2)):
         for j in range(int(len(arr)/2)):
             sum += max([arr[i][j], arr[i][maxInd - j], arr[maxInd - i][j], arr[maxInd - i][maxInd - j]])
